9094596c9e8113cba511e66f22baa1bd313096aa
mahesh <deshmukh.mahesh.n@gmail.com>
2024-04-04 19:43:03 +0530

    workflow updated

--- src/containers/user/index.js
+++ src/containers/user/index.js
@@ -7,6 +7,8 @@ export const UserData = () => {
   return (
     <>
       <Container>
+        <h2>User's Data</h2>
+        <hr className="my-4" />
         <CreateUser />
         <hr className="my-4" />
         <UserList />
